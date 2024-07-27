da361e5c8b7060fd3d71beb5547aec4252a4c628
Martin Pecen <thematinak@gmail.com>
2024-07-27 14:30:28 +0200

    Add delete functionality

--- src/main/java/org/example/orm/manipulator/SqlDelete.java
+++ src/main/java/org/example/orm/manipulator/SqlDelete.java
@@ -1,8 +1,40 @@
-package org.example.orm.selector;
+package org.example.orm.manipulator;
+
+import org.example.orm.annotation.IdColumn;
+import org.example.orm.annotation.Table;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.lang.reflect.Field;
 
 public class SqlDelete {
 
-    public static void delete(Object entity) {
+    public static void delete(JdbcTemplate jdbcTemplate, Object entity) {
+        Table table = SqlAnnotationGetter.getTableAnnot(entity);
 
+        String idColumnName = null;
+        Object idVal = null;
+        for (Field field : entity.getClass().getFields()) {
+            IdColumn idColumn = field.getAnnotation(IdColumn.class);
+            if (idColumn != null) {
+                idColumnName = idColumn.value();
+                idVal = getDataFromField(field, entity);
+                break;
+            }
+        }
+
+        if (idColumnName == null) {
+            throw new RuntimeException("@IdColumn annotation was not found on object: " + entity.getClass());
+        }
+
+        String sql = "Delete from " + table.value() + " where " + idColumnName + " = ?";
+        jdbcTemplate.update(sql, idVal);
+    }
+
+    private static Object getDataFromField(Field field, Object entity) {
+        try {
+            return field.get(entity);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
